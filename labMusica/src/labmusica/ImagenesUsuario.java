b809e9e6f3b17140e73d7d1d3644ccb5b798d17d
ElkinC21 <elkin.leo.cruz21@gmail.com>
2025-06-20 18:04:28 +0200

    Clase imagenes

---
+++ labMusica/src/labmusica/ImagenesUsuario.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package labmusica;
+
+import java.io.File;
+import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+
+public class ImagenesUsuario {
+   
+    public static ImageIcon chooseImage(JFrame parent) {
+       
+        JFileChooser eligirimg = new JFileChooser();
+       
+        FileNameExtensionFilter tipo = new FileNameExtensionFilter("Imagenes (*.jpg, *.jpeg, *.png, *.gif)","jpg", "jpeg", "png", "gif");
+        eligirimg.setFileFilter(tipo);
+        eligirimg.setAcceptAllFileFilterUsed(false); 
+
+       
+        int resultado = eligirimg.showOpenDialog(parent);
+        if (resultado == JFileChooser.APPROVE_OPTION) {
+            File dato = eligirimg.getSelectedFile();
+            
+            return new ImageIcon(dato.getAbsolutePath());
+        } else {
+           
+            return null;
+        }
+    }
+  
+}
